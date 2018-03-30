5bec88822951bb16c8e098ec257692dceecce078
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-29 21:17:31 -0500

    added data and cards

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log("hello");
+
+const writeToDom = (string, id) => {
+    document.getElementById(id).innerHTML = string;
+};
+
+const planetCardBuilder = (planetArray) => {
+        let planetString = "";
+    for (let i=0; i<planetArray.length; i++) {
+        planetString += `<div class="planet-card">`;
+        planetString += `<h2>${planetArray[i].name}</h2>`;
+        planetString += `</div>`;
+    }
+    writeToDom(planetString, "planet-card-holder");
+};
+
+
+function xhrCall () {
+   const data = JSON.parse(this.responseText);
+   planetCardBuilder(data.planets);
+};
+
+function doesNotWork() {
+    console.log("you failed at life");
+};
+
+const startTheThing = () => {
+    let request = new XMLHttpRequest();
+    request.addEventListener("load", xhrCall);
+    request.addEventListener("error", doesNotWork);
+    request.open("GET", "planets.json");
+    request.send();
+}
+
+startTheThing();
+
+// name, imageURL, description, isGasPlanet, numberOfMoons, nameOfLargestMoon
